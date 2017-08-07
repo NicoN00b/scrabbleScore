5b900f5cd2b90c42a910f3c906c9e8ba709cd5f6
Beth Hansen and Nicholas Raethke <nicholas@>
2017-08-07 15:33:29 -0700

    add a image

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,24 +1,45 @@
 import scrabble.Scrabble;
 
-import java.io.BufferedReader;
+import spark.ModelAndView;
+//import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+//import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//
+//        System.out.println("I can validate your Scrabble score");
+//        System.out.println("Enter your word:");
 
-        System.out.println("I can validate your Scrabble score");
-        System.out.println("Enter your word:");
+        staticFileLocation( "/public");
 
-        try {
-            String inputtedWord = bufferedReader.readLine();
+        get("/", (request, response) ->
+        {   Map<String, Object> model = new HashMap<>();
+        return new ModelAndView(model, "scrabble.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/score", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String inputtedWord = request.queryParams("inputtedWord");
+
+//        try {
+            //String inputtedWord = bufferedReader.readLine();
             Scrabble newScrabble = new Scrabble();
             int scrabbleResult = newScrabble.calculateScore(inputtedWord);
-            System.out.println(scrabbleResult);
+//            System.out.println(scrabbleResult);
 
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }
+            model.put("inputtedWord", scrabbleResult);
+            return new ModelAndView(model, "score.hbs");
+
+        }, new HandlebarsTemplateEngine());
     }
 }
